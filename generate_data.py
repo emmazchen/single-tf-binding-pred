84ed9fd1a2c8e3935e9b9fc2a7f23d74770f3159
emmazchen <emmazhangchenn@gmail.com>
2023-05-31 16:27:35 -0400

    Rewrote data generation to generate prob and binary data in one go

--- generate_data.py
+++ generate_data.py
@@ -9,9 +9,15 @@ from utils.utils_generate_data import *
 
 
 """ 
-Datasets generated: 
-- dataset with .01 (1%) threshold: balanced_set         length: 4608
-- dataset with 0 threshold: balanced_set_no_round       length: 331776
+4 totala datasets generated: 
+
+    2 probability datasets:
+    - dataset with .01 (1%) threshold: prob_set_r       length: 4608
+    - dataset with 0 threshold: prob_set_nr             length: 331776
+
+    2 binary datasets:
+    - dataset with .01 (1%) threshold: binary_set_r     length: 4608
+    - dataset with 0 threshold: binary_set_nr           length: 331776
 
     1% threshold means that if frequency of particular nucleotide at particular position is less than 1%, we disregard it and consider freq of that nucleotide to be 0 at that psoition
 
@@ -32,81 +38,91 @@ dna_matrices = len_n_dna_matrices(10)
 
 """ Get PFM as array """
 
-with open(f'/data/ezc2105/tf-binding-pred-pfm/MA0048.2.jaspar') as handle:
+with open('MA0048.2.jaspar') as handle:
     pfm_record = read(handle, 'jaspar')
 
 pfm = jaspar_record_to_array(pfm_record)
 
 # normalize
 normalized_pfm = np.divide(pfm, np.sum(pfm, axis=0))
+pfm_nr = normalized_pfm
 
 # round down. anything below .01 (1%) rounded to 0
-rounded_pfm = np.round(normalized_pfm, 2)                       #can try alternatives -> no rounding or higher threshold
+pfm_r = np.round(normalized_pfm, 2)
 
 
-"""Assess whether they bind """
-# Logic: Multiply element-wise with pfm matrix.
-# In pfm (binary allowed/unallowed, binding/nonbinding), unallowed/nonbinding nucleotides are coded as 1.
-# After multiplication, sum everything, and if zero, it's bind. If it's nonzero, it's no-bind -> it won't bind even if one nucleotide mismatches
-# Invert to get 1 for bind, 0 for no-bind
+"""Assess whether they bind -> we do this first with rounded (r) """
+# Logic: Multiply element-wise with pfm matrix, sum along axis 0 (col) to find prob of that position, get product along axis of nucleotides positions to find prob of overall seq
 
 # Assumption: all possible combo of allowed/binding nucleotides at different positions are binding combos -> if A and C are allowed for binding in pos 1, A and C are allowed for binding in pos 2, assume AA, AC, CA, CC are all possible for binding
 # Otherwise, positive set of binding DNA seq will be even smaller
 
+# get probability of binding
+labels_r = [ np.prod( np.sum( np.multiply(matrix, pfm_r), axis=0) ) for matrix in dna_matrices]
 
-pfm_mask = np.equal(rounded_pfm, 0) # True/1s in pfm_mask are non-binding nucleotides
+# combine dna seq, dna one hot array, and prob into one dataset
+fullset_r = [list(tuple) for tuple in zip(sequences, dna_matrices, labels_r)]
 
-nobindis1_labels = [ np.sum(np.multiply(matrix, pfm_mask)) for matrix in dna_matrices]    # nonzero is no-bind, 0 is bind
-labels = [not label for label in nobindis1_labels] # 1 is bind, 0 is no-bind
-
-# full dataset 
-fullset = [list(tuple) for tuple in zip(sequences, dna_matrices, labels)]
-
-
-
-""" Create balanced dataset """
 # positive dataset
-positive_set = list(filter(lambda x: x[2]==True, fullset))
-print(len(positive_set)) # 4*4*1*1*3*3*1*1*4*4 = 2304 binding sequences
+positive_set_r = list(filter(lambda x: x[2]!=0, fullset_r))
+print(len(positive_set_r)) # 4*4*1*1*3*3*1*1*4*4 = 2304 binding sequences
 
 # randomly sample negative dataset to get balanced set
-negative_set = list(filter(lambda x: x[2]==False, fullset))
-negative_subset = sample(negative_set, len(positive_set))
+negative_set_r = list(filter(lambda x: x[2]==0, fullset_r))
+negative_subset_r = sample(negative_set_r, len(positive_set_r))
 
-balanced_set = positive_set + negative_subset
+# generate probability of binding dataset
+prob_set_r = positive_set_r + negative_subset_r
+
+# generate binary 1-bind 0-nobind dataset
+positive_binary_set_r = [[x,y,1] for [x,y,z] in positive_set_r]
+binary_set_r = positive_binary_set_r + negative_subset_r
 
 
 
+""" Repeat for no round (nr) """
+# get probability of binding
+labels_nr = [ np.prod( np.sum( np.multiply(matrix, pfm_nr), axis=0) ) for matrix in dna_matrices]
 
-""" Repeat to create alternative set without rounding """
-pfm_noround = normalized_pfm
-pfm_mask_noround = np.equal(pfm_noround, 0)
-nobindis1_labels_noround = [np.sum(np.multiply(matrix, pfm_mask_noround)) for matrix in dna_matrices]    # 1 is no-bind, 0 is bind
-labels_noround = [not label for label in nobindis1_labels_noround] # 1 is bind, 0 is no-bind
+# combine dna seq, dna one hot array, and prob into one dataset
+fullset_nr = [list(tuple) for tuple in zip(sequences, dna_matrices, labels_nr)]
 
-fullset_noround = [list(tuple) for tuple in zip(sequences, dna_matrices, labels_noround)]
+# positive dataset
+positive_set_nr = list(filter(lambda x: x[2]!=0, fullset_nr))
+print(len(positive_set_nr)) # 4*4*1*1*3*3*1*1*4*4 = 2304 binding sequences
 
-positive_set_noround = list(filter(lambda x: x[2]==True, fullset_noround))
-print(len(positive_set_noround)) # 4*4*2*3*3*3*3*4*4*4 = 165888 binding sequences
+# randomly sample negative dataset to get balanced set
+negative_set_nr = list(filter(lambda x: x[2]==0, fullset_nr))
+negative_subset_nr = sample(negative_set_nr, len(positive_set_nr))
 
-negative_set_noround = list(filter(lambda x: x[2]==False, fullset_noround))
-negative_subset_noround = sample(negative_set_noround, len(positive_set_noround))
+# generate probability of binding dataset
+prob_set_nr = positive_set_nr + negative_subset_nr
 
-balanced_set_noround = positive_set_noround + negative_subset_noround
-
+# generate binary 1-bind 0-nobind dataset
+positive_binary_set_nr = [[x,y,1] for [x,y,z] in positive_set_nr]
+binary_set_nr = positive_binary_set_nr + negative_subset_nr
 
 
 """ Save as pt """
-torch.save(balanced_set, 'balanced_set.pt')
-torch.save(balanced_set_noround, 'balanced_set_noround.pt')
-
+torch.save(prob_set_r, 'prob_set_r.pt')
+torch.save(prob_set_nr, 'prob_set_nr.pt')
+torch.save(binary_set_r, 'binary_set_r.pt')
+torch.save(binary_set_nr, 'binary_set_nr.pt')
 
 """ Save as csv """
-with open('balanced_set.csv', 'w') as f:
+with open('prob_set_r.csv', 'w') as f:
     write = csv.writer(f)
-    write.writerows(balanced_set)
+    write.writerows(prob_set_r)
 
-with open('balanced_set_noround.csv', 'w') as f:
+with open('prob_set_nr.csv', 'w') as f:
     write = csv.writer(f)
-    write.writerows(balanced_set_noround)
+    write.writerows(prob_set_nr)
+
+with open('binary_set_r.csv', 'w') as f:
+    write = csv.writer(f)
+    write.writerows(binary_set_r)
+
+with open('binary_set_nr.csv', 'w') as f:
+    write = csv.writer(f)
+    write.writerows(binary_set_nr)
 # %%
